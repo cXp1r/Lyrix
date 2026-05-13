fb10fd62097e78e7f7f3c9526a2c6675f1513245
cXp1r <2510037056@qq.com>
2026-05-13 20:11:49 +0800

    feat: 针对applemusic重写比较机制

--- src/searchers/applemusic.rs
+++ src/searchers/applemusic.rs
@@ -62,9 +62,81 @@ impl ISearcher for ApplemusicSearcher {
 
         Ok(results)
     }
-    fn min_score(&self) -> i8 { 5 }
-    fn get_split_char(&self) -> char {
-        '、'
+    fn compare_track(&self, track: &dyn ITrackMetadata, result: &dyn ISearchResult) -> i8 {
+        let mut score = 0i8;
+
+        // 第一步没必要覆写,强制留着了
+        let track_title = track.title().unwrap_or_default().to_lowercase();
+        let result_title = result.title().to_lowercase();
+        if !track_title.is_empty() && !result_title.is_empty() {
+            if track_title == result_title {
+                score += 4;
+            } else if result_title.contains(&track_title) || track_title.contains(&result_title) {
+                score += 2;
+            } else {
+                let clean_track = self.clean_title(&track_title);
+                let clean_result = self.clean_title(&result_title);
+                if clean_track == clean_result {
+                    score += 3;
+                } else if clean_result.contains(&clean_track) || clean_track.contains(&clean_result) {
+                    score += 1;
+                }
+            }
+        }
+        //println!("{}:{}",result_title,score);
+
+        // Artist match
+        let d: Vec<String> = track
+            .artist()
+            .unwrap_or(vec!["",""])   //防止下面崩溃
+            .split("—")
+            .map(|s| s.trim().to_lowercase())
+            .filter(|s| !s.is_empty())
+            .collect();
+        let artists: Vec<String> = d[0].split("、")
+            .map(|s| s.trim().to_lowercase())
+            .filter(|s| !s.is_empty())
+            .collect();
+        for a in &artists {
+            if result.artists().iter().any(|b| {
+                let b = b.to_lowercase();
+                a == &b || a.contains(&b) || b.contains(a)
+            }) {
+                score += 1;
+            }
+        }
+
+        //println!("{} {}",result.artists().join("||"),score);
+        // Album match
+        let track_album = d[1];
+        let result_album = result.album().to_lowercase();
+        if !track_album.is_empty() && !result_album.is_empty() && track_album == result_album {
+            score += 1;
+        }
+
+        //println!("{} {}",result_album,score);
+        // Album artist match
+        let track_album_artist = self.clean_title(&track.album_artist().unwrap_or_default().to_lowercase());
+        let result_album_artist = result.album_artists().unwrap_or_default().to_vec();
+
+        if result_album_artist.iter().any(|s:&String| s.contains(&track_album_artist)) {
+            score += 1;
+        }
+        //println!("(kugou) score:{}",score);
+        if let Some(duration_ms) = track.duration_ms() {
+            if let Some(result_duration_ms) = result.duration_ms() {
+                let diff = duration_ms as i64 - result_duration_ms as i64;
+                if diff == 0 { // 完全匹配
+                    
+                    score += 2;
+                }else if diff <= 1000 { // 1秒内认为时长匹配
+                    score += 1;
+                }
+                
+            }
+        }
+        //println!("{} {}\n",result.duration_ms().unwrap_or_default(),score);
+        score
     }
 }
 
