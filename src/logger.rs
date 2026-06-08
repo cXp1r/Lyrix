195d597e977a4a3312a3e0150c1af044f6414db7
cXp1r <2510037056@qq.com>
2026-06-09 02:29:51 +0800

    refactor: 提高日志格式化可读性

--- src/logger.rs
+++ src/logger.rs
@@ -176,7 +176,7 @@ where
         .duration_since(UNIX_EPOCH)
         .unwrap_or_default()
         .as_secs();
-    let line = format!("[{}][{}][{}] {}", tag, level, now, message);
+    let line = format!("[{}][{}][{}] {}", level, tag, now, message);
     let _ = get_sender().send(LogMsg::Line(line));
 }
 
